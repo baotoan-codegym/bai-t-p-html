b3b68cfc0874db1cc9a6531d2503071580bb09d7
baotoan-codegym <thaibaotoanht1992@gmail.com>
2021-03-10 10:10:36 +0700

    up

---
+++ bt2biendulieutt.js
@@ -0,0 +1,6 @@
+let a = prompt('nhap gia tri do C');
+let b =prompt('nhap gia tri  do F');
+a = parseInt(a);
+b = parseInt(b);
+a = (b-32)*5/9;
+alert(a);
---
+++ bt3dltt.js
@@ -0,0 +1,7 @@
+let r = prompt('nhap ban kinh');
+    const pi = 3.14;
+r = parseInt(r);
+    let area = r*r*pi ;
+    alert(area);
+let Circuit = 2*r*pi;
+alert(Circuit)
